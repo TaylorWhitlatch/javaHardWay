a839e6e9a5808fc6e8d3db50dd05176bb765671e
whit121 <Taylorwhitlatch112@gmail.com>
2018-01-31 12:22:13 -0500

    update

---
+++ Ch2Sample.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class Ch2Sample {
+	public static void main( String[] args ){
+		JFrame myWindow;
+		 myWindow = new JFrame();
+
+		 myWindow.setSize(300, 200);
+		 myWindow.setTitle( "My first Java Program");
+		 myWindow.setVisible(true);
+		 
+	}
+}
---
+++ HelloWorldSwing.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame ; // I m p o r t c l a s s n a m e s
+import java.awt.Graphics ;
+import java.awt.Canvas ;
+public class HelloWorldSwing extends Canvas // C l a s s h e a d e r
+{
+// S t a r t o f b o d y
+	public void paint ( Graphics g )
+// T h e p a i n t m e t h o d
+{
+	g.drawString("Hello, World!",10, 10);
+} // E n d o f p a i n t
+	public static void main ( String [] args ){
+		HelloWorldSwing c = new HelloWorldSwing() ;
+		JFrame f = new JFrame() ;
+		f.add(c) ;
+		f.setSize( 150, 50 ) ;
+		f.setVisible(true) ;
+}
+}
