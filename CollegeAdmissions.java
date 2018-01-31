b922ec9c49fef0317a32495f262dd12dc2c9f562
whit121 <Taylorwhitlatch112@gmail.com>
2018-01-31 11:41:53 -0500

    Update

---
+++ CollegeAdmissions.java
@@ -0,0 +1,31 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class CollegeAdmissions {
+	public static void main( String[] args ){
+		Scanner keyboard = new Scanner(System.in);
+		int math;
+
+		out.println( "Welcome to the Admissions Interface!" );
+		out.print( "Please enter your SAT math score (200-800): ");
+		math = keyboard.nextInt();
+
+		out.print( "Admittance Status: " );
+
+		if ( math >= 790 )
+			out.print( "Certain" );
+		else if ( math >= 700 )
+			out.print( "Safe" );
+		else if ( math >= 580 )
+			out.print( "Probable" );
+		else if ( math >= 500 )
+			out.print( "Uncertain" );
+		else if ( math >= 390 )
+			out.print( "Unlikely " );
+		else 
+			out.print( "Denied ");
+
+		out.println();
+
+	}
+}
